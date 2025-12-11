742576e3278ffb38b62002d596d5a02dfa4abe99
Min Khant Kyaw <mkhantk102@gmail.com>
2025-12-12 01:15:43 +0630

    Add smooth nav with gsap

---
+++ src/app/components/scroll.js
@@ -0,0 +1,16 @@
+import gsap from "gsap";
+import ScrollToPlugin from "gsap/src/ScrollToPlugin";
+gsap.registerPlugin(ScrollToPlugin);
+
+const handleNav = (e, id) => {
+	// use gsap to nav
+	const target = document.getElementById(id);
+	e.preventDefault();
+	gsap.to(window, {
+		scrollTo: target,
+		duration: 0.3,
+		ease: "power3.inOut",
+	});
+};
+
+export default handleNav;
